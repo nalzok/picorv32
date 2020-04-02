184494678f50d24a30abe3aef44c50f4cdac4f39
Qingyao Sun <sunqingyao19970825@icloud.com>
2020-04-02 15:58:34 -0600

    UART echo for TinyFPGA-BX

--- picosoc/tinyfpga.v
+++ picosoc/tinyfpga.v
@@ -42,14 +42,6 @@ module tinyfpga (
 	inout  flash_io3
 );
     ///////////////////////////////////
-    // Disable USB
-    ///////////////////////////////////
-    assign pin_pu = 1'b0;
-    assign pin_usbp = 1'b0;
-    assign pin_usbn = 1'b0;
-
-
-    ///////////////////////////////////
     // Power-on Reset
     ///////////////////////////////////
 	reg [5:0] reset_cnt = 0;
@@ -154,4 +146,50 @@ module tinyfpga (
 		.iomem_wdata  (iomem_wdata ),
 		.iomem_rdata  (iomem_rdata )
 	);
+
+
+    ///////////////////////////////////
+    // Setup USB
+    ///////////////////////////////////
+
+    wire clk_48mhz;
+    wire clk_locked;
+      
+    // Use an icepll generated pll
+    pll pll48( .clock_in(clk), .clock_out(clk_48mhz), .locked( clk_locked ) );
+
+    // Generate reset signal for 46mhz CLK
+    reg [5:0] reset_cnt_48mhz = 0;
+    wire reset_48mhz = ~reset_cnt_48mhz[5];
+    always @(posedge clk_48mhz)
+        if ( clk_locked )
+            reset_cnt_48mhz <= reset_cnt_48mhz + reset_48mhz;
+
+    // uart pipeline in
+    wire [7:0] uart_in_data;
+    wire       uart_in_valid;
+    wire       uart_in_ready;
+
+    // usb uart - this instanciates the entire USB device.
+    usb_uart uart (
+        .clk_48mhz  (clk_48mhz),
+        .reset      (reset_48mhz),
+
+        // pins
+        .pin_usb_p( pin_usbp ),
+        .pin_usb_n( pin_usbn ),
+
+        // uart pipeline in
+        .uart_in_data( uart_in_data ),
+        .uart_in_valid( uart_in_valid ),
+        .uart_in_ready( uart_in_ready ),
+
+        .uart_out_data( uart_in_data ),
+        .uart_out_valid( uart_in_valid ),
+        .uart_out_ready( uart_in_ready  )
+    );
+
+    // USB Host Detect Pull Up
+    assign pin_pu = 1'b1;
+
 endmodule
